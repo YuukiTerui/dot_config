cb1c8d8bf1922d10a471914bb1715fff3e3f4678
YuukiTerui <yuukiterui@YuukiTeruinoMacBook-Pro.local>
2026-05-21 05:57:51 +0900

    manage config

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,23 @@
+if status is-interactive
+# Commands to run in interactive sessions can go here
+end
+
+
+# Alias
+## Git
+alias g='git'
+alias gb='git branch'
+alias gc='git commit'
+alias gco='git checkout'
+alias ga='git add'
+alias gpush='git push'
+alias gpl='git pull'
+
+# Fish
+alias hs='history search'
+alias ths='thistory search'
+
+
+# Starship
+starship init fish | source
+
